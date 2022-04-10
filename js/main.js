ce45a6170be675373856b591a6639b5c3d510c80
Mukti DJ <muktidwijatmoko@gmail.com>
2022-04-10 11:26:35 +0700

    Give audio & edit position

--- js/main.js
+++ js/main.js
@@ -4,16 +4,20 @@ var colors = ["yellow", "green"];
 
 function frame() {
   confetti({
-    particleCount: 2,
-    angle: 60,
-    spread: 55,
+    particleCount: 1,
+    // angle: 60,
+    // spread: 55,
+    angel: 50,
+    spread: 110,
     origin: { x: 0 },
     colors: colors,
   });
   confetti({
-    particleCount: 2,
-    angle: 120,
-    spread: 55,
+    particleCount: 1,
+    // angle: 120,
+    // spread: 55,
+    angel: 140,
+    spread: 110,
     origin: { x: 1 },
     colors: colors,
   });
